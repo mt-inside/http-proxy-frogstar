34809bef0c92e7c7df0650f8c86803cc6f0f59a3
Matt Turner <matturner@gmail.com>
2017-08-11 19:29:18 +0100

    Useful to be able to ctrl-c the fucker

--- run_docker_debug.sh
+++ run_docker_debug.sh
@@ -1,1 +1,1 @@
-docker run --name http-proxy --rm --publish 80:80 --publish 443:443 -v/var/www/ssl:/etc/letsencrypt --network service_net http-proxy
+docker run -it --name http-proxy --rm --publish 80:80 --publish 443:443 -v/var/www/ssl:/etc/letsencrypt --network service_net http-proxy
